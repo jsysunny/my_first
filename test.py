b80c708aedb97e5f6cfd3941742756d79df52a04
jsysunny <sunny011009.jsy@gmail.com>
2025-01-17 22:06:09 +0900

    mex1

---
+++ test.py
@@ -0,0 +1,14 @@
+"""import mex1
+
+p2=mex1.Cvalue()
+p2.add(11)
+p2.add(21)
+p2.add(31)
+p2.fprint()
+value=mex1.plus(10,20)
+print(value)
+"""
+from mex1 import plus
+
+value=plus(10,20)
+print(value)
